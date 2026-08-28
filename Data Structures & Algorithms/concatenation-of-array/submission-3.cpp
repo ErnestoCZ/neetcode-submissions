1ffd33269459af6184c04f080a5c9f9b8f8353bd
Ernest Jerzy Czechowski <58993541+ErnestoCZ@users.noreply.github.com>
2026-08-28 07:50:42 +0200

    Add: concatenation-of-array - submission-3

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-3.cpp
@@ -0,0 +1,17 @@
+#include <cstring>
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans;
+        ans.resize(nums.size() * 2);
+        nums.shrink_to_fit();
+
+        // std::memcpy((int*)&ans[0], (int*)&nums[0], nums.size() * sizeof(int));
+        // std::memcpy((int*)&ans[nums.size()], (int*)&nums[0], nums.size() * sizeof(int));
+    
+        for(size_t index = 0; index < ans.size(); index++){
+            ans[index] = nums[index % nums.size()];
+        }
+        return ans;
+    }
+};
